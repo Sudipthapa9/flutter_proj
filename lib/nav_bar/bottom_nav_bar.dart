d1f0163ef075c816fe9af20dd5d8978da6941ecc
Sudip Thapa <kazit8119@gmail.com>
2026-01-17 22:01:55 +0545

    Add nav bar

---
+++ lib/nav_bar/bottom_nav_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_proj/screen/cart.dart';
+import 'package:flutter_proj/screen/product.dart';
+import 'package:flutter_proj/controller/nav_bar_controller.dart';
+import 'package:flutter_proj/screen/profile.dart';
+import 'package:flutter_proj/screen/search.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NavBarPage extends StatelessWidget {
+  const NavBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final NavBarController userController = Get.put(NavBarController());
+    final pages = [
+      const HomeScreen(),
+      const SearchScreen(),
+      const CartScreen(),
+      const ProfileScreen(),
+    ];
+    return Obx(
+      () => Scaffold(
+        body: IndexedStack(index: userController.index.value, children: pages),
+        //body: pages[userController.index.value],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: userController.index.value,
+          onTap: userController.changeIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.money), label: 'Cart'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
+      ),
+    );
+  }
+}
